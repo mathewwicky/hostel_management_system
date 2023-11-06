8fbde4fbdf8752fe1a7ea08653816f87331e05d9
mathew <mathewwicky8@gmail.com>
2023-11-06 14:53:15 +0300

    serializers file for converting model to json

--- hostel/views.py
+++ hostel/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import HostelSerializer
+from .models import Hostel
 
 # Create your views here.
+
+class HostelView(viewsets.ModelViewSet):
+    serializer_class = HostelSerializer
+    queryset = Hostel.objects.all()
