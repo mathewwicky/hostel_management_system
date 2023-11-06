e7bc58833e52663bf3930099091c34c03cbce6a1
mathew <mathewwicky8@gmail.com>
2023-11-06 15:22:15 +0300

    bookingview/serializers

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import BookingSerializer
+from .models import Booking
 
 # Create your views here.
+
+class BookingView(viewsets.ModelViewSet):
+    serializer_class = BookingSerializer
+    queryset = Booking.objects.all()
--- hms/urls.py
+++ hms/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from hostel import views as hostel_views
+from booking import views as booking_views
+
 
 router = routers.DefaultRouter()
-router.register(r'hostels', hostel_views.HostelView, 'hostels') 
+router.register(r'hostels', hostel_views.HostelView, 'hostels')
+router.register(r'bookings', booking_views.BookingView, 'booking') 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
