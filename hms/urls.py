f545e4aeb6fe7b35ef2826f74e76ff30b1328915
mathew <mathewwicky8@gmail.com>
2023-11-07 05:45:09 +0300

    added the api url for the auth

--- hms/urls.py
+++ hms/urls.py
@@ -3,12 +3,18 @@ from django.urls import path, include
 from rest_framework import routers
 from hostel import views as hostel_views
 from booking import views as booking_views
+from users.views import CustomUserViewSet
+
 
 
 router = routers.DefaultRouter()
 router.register(r'hostels', hostel_views.HostelView, 'hostels')
 router.register(r'bookings', booking_views.BookingView, 'booking') 
+router.register(r'custom-users', CustomUserViewSet)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
 ]
